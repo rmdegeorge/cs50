2467eefa5f6687cc5755c3f7f379771e4264a382
Rob DeGeorge <rmdegeorge@gmail.com>
2020-10-23 07:45:14 -0600

    examples from first python lecture

---
+++ week06/cough.py
@@ -0,0 +1,8 @@
+def main():
+    cough(4)
+
+def cough(n):
+    for i in range(n):
+        print("cough")
+
+main()
---
+++ week06/string.py
@@ -0,0 +1,5 @@
+
+import cs50
+
+s = get_string("What's your name?\n")
+print("hello, " + s)
